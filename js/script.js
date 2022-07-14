08bd5737eaeb30ff82ce64959f78ddc5597c25d3
babalolaisrael <unendingch@gmail.com>
2022-07-15 00:58:08 +0100

    the js folder

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* =================================== typing animation ============================= */
+var typed = new Typed(".typing",{
+    strings:[" ","Web Designer","web Developer","Graphics Designer","Digital Marketer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
